c08f2722c4c3056eb80774a4be942ff2d0844745
Stepanyan <stepan.stepanyan.01@mail.ru>
2021-05-13 15:32:27 +0500

    Разработчик Степанян дополнил мейн класс. Теперь в мейн классе добавлена возможность вывода расчётов на Result.jsp

--- src/main/java/team4/Main.java
+++ src/main/java/team4/Main.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import team4.DliaSotrudnikov;
+import team4.DliaZaiomshikov;
+import team4.Standart;
+
 @WebServlet(name="Calc", urlPatterns="/JavaCalc")
 
 
@@ -26,6 +30,10 @@ public class Main extends HttpServlet{
 		private final String uslovie;
 		private final String dosr;
 		
+		private float proc;
+		private float mes;
+		private float pereplata;
+		private float itog;
 
 						
 		private RequestCalc (String summa, String mesyac, String uslovie, String dosr) {
@@ -49,10 +57,50 @@ public class Main extends HttpServlet{
 			int uslovie_try;
 			int dosr_try;
 			
+			
 		
 			
 			summa_try = Integer.parseInt(summa);
 			mesyac_try = Integer.parseInt(mesyac);
 			uslovie_try = Integer.parseInt(uslovie);
 			dosr_try = Integer.parseInt(dosr);
+			
+			if (uslovie_try == 0) {
+				Standart uslovie = new Standart();
+				
+				itog = uslovie.calculation(summa_try, mesyac_try, dosr_try);
+				proc = uslovie.procc();
+				mes = uslovie.mess();
+				pereplata = uslovie.perepl();
+				request.setAttribute("itog", itog);
+				request.setAttribute("proc", proc);
+				request.setAttribute("mes", mes);
+				request.setAttribute("pereplata", pereplata);
+			}
+			else if (uslovie_try == 1) {
+				DliaZaiomshikov uslovie = new DliaZaiomshikov();
+				itog = uslovie.calculation(summa_try, mesyac_try, dosr_try);
+				proc = uslovie.procc();
+				mes = uslovie.mess();
+				pereplata = uslovie.perepl();
+				request.setAttribute("itog", itog);
+				request.setAttribute("proc", proc);
+				request.setAttribute("mes", mes);
+				request.setAttribute("pereplata", pereplata);
+			}
+			else if (uslovie_try == 2) {
+				DliaSotrudnikov uslovie = new DliaSotrudnikov();
+				itog = uslovie.calculation(summa_try, mesyac_try, dosr_try);
+				proc = uslovie.procc();
+				mes = uslovie.mess();
+				pereplata = uslovie.perepl();
+				request.setAttribute("itog", itog);
+				request.setAttribute("proc", proc);
+				request.setAttribute("mes", mes);
+				request.setAttribute("pereplata", pereplata);
+			}
 		}
+	}
+}
+		
+	
