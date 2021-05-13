fd5130a8f4da5adafef97d5406fbc88cca4e1a8e
Sheifel <janna-sch@mail.ru>
2021-05-13 15:15:39 +0500

    Разработчик Шейфель Жанна добавила класс на основе абстрактного,который рассчитывает платёж по условиям для партнёров.

---
+++ src/main/java/team4/DliaSotrudnikov.java
@@ -0,0 +1,41 @@
+package team4;
+
+	public class DliaSotrudnikov extends Uslovie {
+		
+		@Override
+		public float calculation(int summa_try, int mesyac_try, int dosr_try) {
+			float procMin; // Объявление переменной минимального процента
+			float procMax; // Объявление переменной максимального процента
+			float procRaznica; // Объявление переменной, которая рассчитывает разницу
+			float procMes; // Объявление переменной месячного процента
+
+			if(summa_try < 250001) { //Условие расчёта процента, который зависит от суммы
+				procMin = (float) 15.99;
+				procMax = (float) 23.49;
+				procRaznica = procMax - procMin;// Расчёт разницы
+			} else if (summa_try < 700001) {//Условие расчёта процента, которая зависит от суммы
+				procMin = (float) 13.99;
+				procMax = (float) 18.99;
+				procRaznica = procMax - procMin;// Расчёт разницы
+			} else {
+				procMin = (float) 11.99;
+				procMax = (float) 18.99;
+				procRaznica = procMax - procMin;// Расчёт разницы
+
+            }
+			procMes = procRaznica/48;// Вычисление ежемесячного процента, для вычисления конечного процента
+			proc = (float) (procMax - procMes*(mesyac_try - 12));
+		    dosr(dosr_try);
+		       this.mproc = proc/1200; // Вычисление ежемесячного процента
+		       this.mesyac = (float) (summa_try*(mproc+(mproc/(((Math.pow(1 + mproc, mesyac_try)))-1)))); // Формула вычисления аннуитетного платежа
+		       this.pereplata = mesyac * mesyac_try - summa_try; // Вычисление переплаты
+		       this.itog = summa_try + pereplata;
+		       itog = (int)(itog*100);
+		       itog = itog/100;
+		       return itog;
+		    
+		       }
+	}
+	
+
+	
