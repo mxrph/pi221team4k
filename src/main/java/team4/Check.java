939270dd78d5dcfe752187a304a0797688464683
mxrph <samson-artem@mail.ru>
2021-05-13 16:50:49 +0500

    edit check

--- src/main/java/team4/Check.java
+++ src/main/java/team4/Check.java
@@ -9,77 +9,59 @@ import java.util.List;
 
 public class Check implements Config {
 
-	private List<String> adminLogins = new ArrayList<>();
-    private List<String> adminPasswords = new ArrayList<>();
+	private List<String> admlog = new ArrayList<>();
+    private List<String> admpass = new ArrayList<>();
     
-    private List<String> userLogins = new ArrayList<>();
-    private List<String> userPasswords = new ArrayList<>();
+    private List<String> usrlog = new ArrayList<>();
+    private List<String> usrpass = new ArrayList<>();
     
-    boolean lp;
+    boolean srav;
 
     public boolean check1(String login, String password) throws IOException {
-    	lp = false;
+    	srav = false;
         setParam();
-        for (int i = 0; i < adminLogins.size(); i++) {
-            if (login.equals(adminLogins.get(i)) && password.equals(adminPasswords.get(i))) {
-                lp = true;
+        for (int i = 0; i < admlog.size(); i++) {
+            if (login.equals(admlog.get(i)) && password.equals(admpass.get(i))) {
+            	srav = true;
                 break;
             }
         }
-        return lp;
+        return srav;
     }
     
     public boolean check2(String login, String password) throws IOException {
-    	lp = false;
-        for (int i = 0; i < userLogins.size(); i++) {
-            if (login.equals(userLogins.get(i)) && password.equals(userPasswords.get(i))) {
-                lp = true;
+    	srav = false;
+        for (int i = 0; i < usrlog.size(); i++) {
+            if (login.equals(usrlog.get(i)) && password.equals(usrpass.get(i))) {
+            	srav = true;
                 break;
             }
         }
-        return lp;
+        return srav;
     }
 
     public void setParam() throws IOException {
-    	ClassLoader classLoader = getClass().getClassLoader();
-    	File file1 = new File(classLoader.getResource("1.txt").getFile());
-
-    	//File is found
-
-
-
-
-    	//File file1 = new File("src/main/resources/2.txt");
-    	FileReader fr1 = new FileReader(file1);
-    	BufferedReader reader1 = new BufferedReader(fr1);
-    	String line;
-    	while((line = reader1.readLine()) != null) {
-    		int end = line.indexOf(' ');
-    		adminLogins.add(line.substring(0, end));
-    		line = line.substring(end+1);
-    		adminPasswords.add(line.substring(0));
-    		}
-    		reader1.close();
-
-    	ClassLoader classLoader1 = getClass().getClassLoader();
-    	File file2 = new File(classLoader1.getResource("2.txt").getFile());
-
-
-
-
-
-
-    	//File file1 = new File("src/main/resources/1.txt");
-    	FileReader fr2 = new FileReader(file2);
-    	BufferedReader reader2 = new BufferedReader(fr2);
-    	String line1;
-    		while((line1 = reader2.readLine()) != null) {
-
-    			int end = line1.indexOf(' ');
-    			adminLogins.add(line1.substring(0, end));
-    			line1 = line1.substring(end+1);
-    	adminPasswords.add(line1.substring(0));
-    }
-    	reader2.close();
+    	File file1 = new File("2.txt");
+        FileReader fr1 = new FileReader(file1);
+        BufferedReader reader1 = new BufferedReader(fr1);
+        String line;
+        while((line = reader1.readLine()) != null) {
+            int end = line.indexOf(' ');
+            admlog.add(line.substring(0, end));
+            line = line.substring(end+1);
+            admpass.add(line.substring(0));
+        }
+        reader1.close();
+        
+        File file2 = new File("1.txt");
+        FileReader fr2 = new FileReader(file2);
+        BufferedReader reader2 = new BufferedReader(fr2);
+        while((line = reader2.readLine()) != null) {
+            int end = line.indexOf(' ');
+            usrlog.add(line.substring(0, end));
+            line = line.substring(end+1);
+            usrpass.add(line.substring(0));
+        }
+        reader2.close();
     }
 }
