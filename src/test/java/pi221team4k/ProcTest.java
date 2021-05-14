18d7533c478a0a69d635ce2d0f75f4c15b03ea7e
Sheifel <janna-sch@mail.ru>
2021-05-15 04:17:11 +0500

    Добавила JUnit тест на проверку процентной ставки

---
+++ src/test/java/pi221team4k/ProcTest.java
@@ -0,0 +1,79 @@
+package pi221team4k;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+	public class ProcTest {
+
+		private int summ;
+
+		private int mes;
+
+@Before
+
+	public void prisvoenieTest() {
+
+			summ = 2500000; // тест 1
+
+			mes = 36;
+
+			
+
+	}
+
+@Test
+
+	public void porcentTest() {
+
+	double procMin;
+
+	double procMax;
+
+	double procRaznica;
+
+	double procMes;
+
+	double proc;
+
+	if(summ < 250001) {
+
+		procMin = 16.99;
+
+		procMax = 23.49;
+
+		procRaznica = procMax - procMin;
+
+	} else if (summ < 700001) {
+
+		procMin = 14.99;
+
+		procMax = 18.99;
+
+		procRaznica = procMax - procMin;
+
+	} else {
+
+		procMin = 11.99;
+
+		procMax = 18.99;
+
+		procRaznica = procMax - procMin;
+
+	}
+
+	procMes = procRaznica/48;
+
+	proc = procMax - procMes*(mes - 12);
+
+	assertEquals(15, (int)proc); // тест 1
+
+	System.out.println(proc);
+
+
+
+	}
+
+}
